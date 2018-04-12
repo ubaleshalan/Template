5b178a98dc7e89838a2516b6e0745c13b347cc46
ubaleshalan <shalan.ubale95>
2018-04-12 10:16:06 +0530

    Creating Template Adding Project Information

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+	
+	constructor(private _http: HttpClient)
+  title = 'app';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule,Routes} from '@angular/router';
+import { FormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { OurTeamComponent } from './pages/our-team/our-team.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+
+const appRoutes: Routes = [
+	{ path: 'Home',component : HomeComponent },
+	{ path: 'AboutUs',component : AboutUsComponent },
+	{ path: 'Courses',component : CoursesComponent },
+	{ path: 'OurTeam',component : OurTeamComponent },
+	{ path: 'ContactUs',component : ContactUsComponent }
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+	
+    AboutUsComponent,
+    CoursesComponent,
+    OurTeamComponent,
+    ContactUsComponent
+  ],
+  imports: [
+    BrowserModule,
+	RouterModule.forRoot(appRoutes),
+	FormsModule,
+	NgbModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
